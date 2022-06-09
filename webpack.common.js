767a13f544d14e59f3053e410cad4612a9e3b04b
Luan Maffra <maffra-oliveira@outlook.com>
2022-06-09 17:08:53 -0300

    feat: initial strutured folders

---
+++ webpack.common.js
@@ -0,0 +1,21 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: './src/app/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[fullhash:8].js',
+    sourceMapFilename: '[name].[fullhash:8].map',
+    chunkFilename: '[id].[fullhash:8].js',
+    publicPath: '/'
+  
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '~': path.join(__dirname, 'src'),
+    }
+  },
+  plugins: [new CleanWebpackPlugin()]
+}
---
+++ webpack.dev.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const path = require('path')
+
+const common = require('./webpack.common')
+
+const { merge } = require('webpack-merge')
+
+module.exports = merge(
+  {...common },
+  {
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.(ts|js)x?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-react",
+                "@babel/preset-typescript",
+              ],
+            },
+          },
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    devtool: 'source-map',
+    devServer: {
+      static: path.join(__dirname, "public"),
+      compress: true,
+      historyApiFallback: true,
+      port: 3000
+    },
+    plugins: [
+      new ForkTsCheckerWebpackPlugin({
+        async: false,
+        typescript: {
+            memoryLimit: 4096,
+            profile: true
+          }
+      }),
+      new HtmlWebpackPlugin({
+          inject: true,
+          template: './template.dev.html'
+        })
+    ],
+})
