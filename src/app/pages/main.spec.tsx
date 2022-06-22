052b42a081b61f8a5d9793ced2a1bdb914ec5c0f
Luan Maffra <maffra-oliveira@outlook.com>
2022-06-22 13:25:35 -0300

    feat: setup jest files

---
+++ src/app/pages/main.spec.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Main } from './main'
+import { render } from '@testing-library/react'
+
+describe('Main', () => {
+  test('Should render main component on screen', () => {
+    const { container } = render(<Main />)
+    expect(container).toBeInTheDocument()
+  })
+})
