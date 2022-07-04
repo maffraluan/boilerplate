8a13bf78ef91627abe0ea402d4a6690c2762fb46
Luan Maffra <maffra-oliveira@outlook.com>
2022-07-04 17:24:44 -0300

    feat: added basic config in route

--- src/app/routes/router.tsx
+++ src/app/routes/router.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 
-import { Main, Character } from '~/app/pages/'
+import { Main, Character, FavoriteCharacter } from '~/app/pages/'
 
 const Router = () => {
   return (
     <Routes>
       <Route path='character' element={<Main />} />
       <Route path='character/:id' element={<Character />} />
+      <Route path='favorite-character' element={<FavoriteCharacter />} />
       <Route path='*' element={<Navigate to='character' />} />
     </Routes>
   )
